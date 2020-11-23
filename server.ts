5b95376a344340e2577d41f5cb1b1d4e8eccf38c
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 13:35:31 +0100

    feat: add properties mock data

--- server.ts
+++ server.ts
@@ -2,6 +2,20 @@ import { serve } from "https://deno.land/std/http/server.ts";
 const port = 8000;
 const s = serve({ port });
 console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`);
+
+const properties = [
+  {
+    id: 'p1',
+    name: 'Property P1',
+    type: 'VILLA'
+  },
+  {
+    id: 'p2',
+    name: 'Property P2',
+    type: 'STUDIO'
+  },
+];
+
 for await (const req of s) {
   const buf: Uint8Array = await Deno.readAll(req.body);
   req.respond({ body: buf });
