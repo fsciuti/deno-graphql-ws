f77db3cf96835f6a028ae2694f3a1043c48befe0
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 13:41:13 +0100

    feat: add graphql vanilla server

--- server.ts
+++ server.ts
@@ -1,4 +1,6 @@
 import { serve } from "https://deno.land/std/http/server.ts";
+import { buildSchema, graphql } from "https://cdn.skypack.dev/graphql@v15.4.0?dts";
+
 const port = 8000;
 const s = serve({ port });
 console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`);
@@ -16,12 +18,37 @@ const properties = [
   },
 ];
 
+const schema = buildSchema(`
+  type Property {
+    id: String
+    name: String
+    type: String
+  }
+  type Query {
+    properties: [Property]
+  }
+`);
+
+const resolvers = {
+  properties: () => {
+    return properties;
+  },
+};
+
 for await (const req of s) {
   switch (req.url) {
     case "/graphql": 
       if (req.method === "POST") {
         // GraphQL Endpoint
-        req.respond({ body: 'GraphQL Endpoint' });
+        const decoder = new TextDecoder("utf-8");
+        const headers = new Headers();
+        headers.append("content-type", "application/json; charset=utf-8");
+
+        const buf : Uint8Array = await Deno.readAll(req.body);
+        const query = JSON.parse(decoder.decode(buf)).query;
+        const response = await graphql(schema, query, resolvers);
+        
+        req.respond({ body: JSON.stringify(response), headers });
         continue;
       }
 
