da0f7bb9e4935cc4ae81f489348081dfc3d16bba
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 14:00:57 +0100

    feat: add oak middleware framework

--- server.ts
+++ server.ts
@@ -1,11 +1,8 @@
-import { serve } from "https://deno.land/std/http/server.ts";
-import { serveFile } from "https://deno.land/std/http/file_server.ts";
+import { Application, Router, send } from 'https://deno.land/x/oak/mod.ts';
 
-import { buildSchema, graphql } from "https://cdn.skypack.dev/graphql@v15.4.0?dts";
+import { buildSchema, graphql, Source } from "https://cdn.skypack.dev/graphql@v15.4.0?dts";
 
 const port = 8000;
-const s = serve({ port });
-console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`);
 
 const properties = [
   {
@@ -37,32 +34,26 @@ const resolvers = {
   },
 };
 
-for await (const req of s) {
-  switch (req.url) {
-    case "/graphql": 
-      if (req.method === "POST") {
-        // GraphQL Endpoint
-        const decoder = new TextDecoder("utf-8");
-        const headers = new Headers();
-        headers.append("content-type", "application/json; charset=utf-8");
+const router = new Router();
+router
+  .get("/", context => {
+    context.response.body = "Welcome to Deno-GraphQL Demo"
+  })
+  .get("/graphql", async context => {
+    await send(context, './graphiql.html');
+  })
+  .post("/graphql", async context => {
+    const requestBody = await context.request.body();
+    const value = await requestBody.value;
+    const query: Source = value.query;
 
-        const buf : Uint8Array = await Deno.readAll(req.body);
-        const query = JSON.parse(decoder.decode(buf)).query;
-        const response = await graphql(schema, query, resolvers);
+    const response = await graphql(schema, query, resolvers);
+    context.response.body =  response;
+  });
 
-        req.respond({ body: JSON.stringify(response), headers });
-        continue;
-      }
+const app = new Application();
+app.use(router.routes(), router.allowedMethods());
 
-      if (req.method === "GET") {
-        const content = await serveFile(req, './graphiql.html');
-        req.respond(content);
-        continue;
-      }
+console.log(`🦕 Deno OAK server running at http://localhost:${port}/ 🦕`);
 
-      req.respond({ status: 405 });
-      break;
-    default:
-      req.respond({ body: 'Welcome to Deno-GraphQL Demo' });
-  }
-}
+await app.listen({ port });
