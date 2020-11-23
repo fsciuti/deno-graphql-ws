0d31d83a5bbd936d6ae8fb884be3ca2d9c9272c5
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 14:17:16 +0100

    feat: add initialize methods

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,12 @@ export class App {
     constructor(public port: number) {
         this.app = new Application();
 
+        this.initializeEventListener();
+        this.initializeMiddlewares();
+        this.initializeRoutes();
+    }
+
+    private initializeEventListener() {
         this.app.addEventListener('listen', ({ hostname, port, secure }) => {
             console.log(`🦕 Deno/GraphQL OAK Server running onc: ${
                 secure ? "https://" : "http://"
@@ -16,6 +22,8 @@ export class App {
             console.error(evt.error);
         });
     }
+    private initializeMiddlewares() {}
+    private initializeRoutes() {}
 
     public async listen() {
         return await this.app.listen({ port: this.port });
