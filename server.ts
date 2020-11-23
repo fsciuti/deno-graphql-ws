a1a4d82de72dd57f4e7307837efa99640dd7429e
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 13:34:52 +0100

    feat: add port constant

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import { serve } from "https://deno.land/std/http/server.ts";
-const s = serve({ port: 8000 });
+const port = 8000;
+const s = serve({ port });
 console.log(`🦕 Deno server running at http://localhost:8000/ 🦕`);
 for await (const req of s) {
   const buf: Uint8Array = await Deno.readAll(req.body);
