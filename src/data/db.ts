af51cbb98119d35da607da8d19793c289c12c357
Francesco Sciuti <francescosciuti@MBPdiFrancesco.homenet.telecomitalia.it>
2020-11-23 15:16:24 +0100

    feat: add and update property resolvers with types

--- src/data/db.ts
+++ src/data/db.ts
@@ -1,18 +1,30 @@
+import { AddPropertyInput, Property } from '../schema/schemaTypes.ts';
+
 const data: any = {
     Properties: [
         {
-        id: 'p1',
-        name: 'Property P1',
-        type: 'VILLA'
+            id: 'p1',
+            name: 'Property P1',
+            type: 'VILLA'
         },
         {
-        id: 'p2',
-        name: 'Property P2',
-        type: 'STUDIO'
+            id: 'p2',
+            name: 'Property P2',
+            type: 'STUDIO'
         },
     ]
 };
 
-export const getProperties = () => {
-    return [...data.Properties];
+export const getProperties = (): Property[] => {
+    return <Property[]>[...data.Properties];
+}
+
+export const getProperty = (propertyId: string): Property => {
+    return <Property>data.Properties.find((item: Property) => propertyId === item.id);
+}
+
+export const addProperty = (property: AddPropertyInput) => {
+    const newProperty: Property = { id: `p${++getProperties().length}`, ...property };
+    data.Properties.push(newProperty);
+    return newProperty;
 }
--- src/resolvers/property.ts
+++ src/resolvers/property.ts
@@ -1,4 +1,5 @@
 import * as db from '../data/db.ts';
+import { MutationAddPropertyArgs, PropertyType } from "../schema/schemaTypes.ts";
 
 export const propertyResolvers = {
     Query: {
@@ -6,8 +7,19 @@ export const propertyResolvers = {
             return db.getProperties();
         }
     },
+    Mutation: {
+        addProperty(_parent: any, { input }: MutationAddPropertyArgs) {
+            const property = db.addProperty(input!);
+            return {
+                code: 200,
+                success: true,
+                message: "Property was added!",
+                property
+            }
+        }
+    },
     PropertyType: {
-        STUDIO: "STUDIO",
-        VILLA: "VILLA"
+        STUDIO: PropertyType.Studio,
+        VILLA: PropertyType.Villa
     }
 }
